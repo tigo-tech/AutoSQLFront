548bd65e18f16fde5ac636ab81b6d21b0d1b2f3a
zsj <15528377112@163.com>
2019-05-21 22:57:21 +0800

    update index.html

--- js/sql_li.js
+++ js/sql_li.js
@@ -12,18 +12,31 @@ var sqlLi = new Vue({
     tableName: 'Item',
     colNames:[
     ],
-    checkedNames:[],
-    // selected:[]
+    // checkedNames:[],
+    // selected:0,
+    pictures:1,
+    aggNum:[]
   },
   methods:{
   	tableGet(val){
   		this.colNames=[]
   		this.tableName=val
-  		this.colNames.push({col:'物品ID'},{col:'物品名称'},{col:'数量'},{col:'物品类别'})
+  		this.colNames.push({col:'物品ID'},{col:'物品名称'},{col:'物品类别'},{col:'数量'})
 
   	},
   	tableSelect(val){
-  		alert(this.checkedNames)
+  		alert(this.aggNum)
+  	},
+  	getId(col){
+  		return "col_"+col
+  	},
+  	changePic(val){
+  		this.pictures = val.target.value
+  	},
+  	changeAgg(val,item){
+  		if (val.target.value != null) {
+  			this.$set(this.aggNum,this.aggNum.length,{item:val.target.value})
+  		}
   	}
 }
 })
